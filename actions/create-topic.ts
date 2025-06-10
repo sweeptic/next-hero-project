e48dbd92444858b6abfb7723526a7bc3fa041e6d
Attila Horvath <sweeptic@gmail.com>
2025-06-10 10:41:44 +0200

    32. Displaying Validation Errors

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -6,7 +6,7 @@ const createTopicSchema = z.object({
   name: z
     .string()
     .min(3)
-    .regex(/^[a-z-]+$/, {
+    .regex(/^[a-z-]/, {
       message: 'Must be lowercase letters or dashes without spaces',
     }),
   description: z.string().min(10),
--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -2,10 +2,10 @@
 import * as actions from '@/actions';
 
 import { Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea } from '@heroui/react';
-import { useFormState } from 'react-dom';
+import { useActionState } from 'react';
 
 export default function TopicCreateForm() {
-    const [formState, action] = useFormState(actions.createTopic, { errors: {} });
+  const [formState, action] = useActionState(actions.createTopic, { errors: {} });
 
   return (
     <Popover>
@@ -17,12 +17,21 @@ export default function TopicCreateForm() {
         <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80 ">
             <h3 className="text-lg">Create Topic</h3>
-            <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
+            />
             <Textarea
               name="description"
               label="Description"
               labelPlacement="outside"
               placeholder="Describe your topic"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
             />
             <Button type="submit">Submit</Button>
           </div>
