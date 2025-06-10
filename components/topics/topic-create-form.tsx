f27c08c5cc68b56b334bfd50e2d5a0a88bd615dc
Attila Horvath <sweeptic@gmail.com>
2025-06-10 13:52:54 +0200

    34. Handling Database Errors in Forms #2

--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -2,10 +2,22 @@
 
 import * as actions from '@/actions';
 import { Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea } from '@heroui/react';
-import { useActionState } from 'react';
+import { useActionState, useEffect, useState } from 'react';
 
 export default function TopicCreateForm() {
   const [formState, action] = useActionState(actions.createTopic, { errors: {} });
+
+  console.log('debug', formState.errors.username?.join(', '));
+
+  const [customFormState, setCustomFormState] = useState({ errors: {} } as any);
+  const [username, setUsername] = useState() as any;
+  const [description, setDescription] = useState() as any;
+
+  useEffect(() => {
+    console.log('formState', formState);
+
+    setCustomFormState(formState);
+  }, [formState]);
 
   return (
     <Popover>
@@ -20,18 +32,28 @@ export default function TopicCreateForm() {
             <Input
               name="username"
               label="Name"
+              value={username}
               labelPlacement="outside"
               placeholder="Name"
-              isInvalid={!!formState.errors.username}
-              errorMessage={formState.errors.username?.join(', ')}
+              onChange={(ev) => {
+                setUsername(ev?.target.value);
+                setCustomFormState({ errors: {} });
+              }}
+              isInvalid={!!customFormState.errors.username}
+              errorMessage={customFormState.errors.username?.join(', ')}
             />
             <Textarea
               name="description"
               label="Description"
+              value={description}
               labelPlacement="outside"
+              onChange={(ev) => {
+                setDescription(ev?.target.value);
+                setCustomFormState({ errors: {} });
+              }}
               placeholder="Describe your topic"
-              isInvalid={!!formState.errors.description}
-              errorMessage={formState.errors.description?.join(', ')}
+              isInvalid={!!customFormState.errors.description}
+              errorMessage={customFormState.errors.description?.join(', ')}
             />
             {formState.errors._form ? (
               <div className="rounded p-2 bg-red-200 border border-red-400">{formState.errors._form?.join(', ')}</div>
