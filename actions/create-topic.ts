5422c9f8dc5318364f244c76470bb588f55495e7
Attila Horvath <sweeptic@gmail.com>
2025-06-10 10:32:42 +0200

    29. Fixing UseFormState Type Errors

--- actions/create-topic.ts
+++ actions/create-topic.ts
@@ -12,7 +12,7 @@ const createTopicSchema = z.object({
   description: z.string().min(10),
 });
 
-export async function createTopic(formData: FormData) {
+export async function createTopic(formState: number, formData: FormData) {
   const result = createTopicSchema.safeParse({
     name: formData.get('name'),
     description: formData.get('description'),
@@ -20,4 +20,6 @@ export async function createTopic(formData: FormData) {
   if (!result.success) {
     console.log('result.error', result.error.flatten().fieldErrors);
   }
+
+  return 10;
 }
--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -2,8 +2,11 @@
 import * as actions from '@/actions';
 
 import { Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea } from '@heroui/react';
+import { useFormState } from 'react-dom';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, 5);
+
   return (
     <Popover>
       <PopoverTrigger>
@@ -11,7 +14,7 @@ export default function TopicCreateForm() {
       </PopoverTrigger>
 
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80 ">
             <h3 className="text-lg">Create Topic</h3>
             <Input name="name" label="Name" labelPlacement="outside" placeholder="Name" />
