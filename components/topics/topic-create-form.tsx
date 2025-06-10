a914c19c030c863ba2dfc6946b2e6d029d9f876d
Attila Horvath <sweeptic@gmail.com>
2025-06-10 10:18:27 +0200

    24. Creating a Popover Form

--- components/topics/topic-create-form.tsx
+++ components/topics/topic-create-form.tsx
@@ -1,3 +1,25 @@
+'use client';
+import * as actions from '@/actions';
+
+import { Button, Input, Popover, PopoverContent, PopoverTrigger, Textarea } from '@heroui/react';
+
 export default function TopicCreateForm() {
-  return <div>Topic Create Form</div>;
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Button color="primary">Create a Topic</Button>
+      </PopoverTrigger>
+
+      <PopoverContent>
+        <form action={actions.createTopic}>
+          <div className="flex flex-col gap-4 p-4 w-80 ">
+            <h3 className="text-lg">Create Topic</h3>
+            <Input label="Name" labelPlacement="outside" placeholder="Name" />
+            <Textarea label="Description" labelPlacement="outside" placeholder="Describe your topic" />
+            <Button type="submit">Submit</Button>
+          </div>
+        </form>
+      </PopoverContent>
+    </Popover>
+  );
 }
