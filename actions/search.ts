279c4c1a78c44f0e2ac4261507b603c8dff2cafb
Attila Horvath <sweeptic@gmail.com>
2025-06-12 14:35:24 +0200

    08. Redirecting From a Server Action

--- actions/search.ts
+++ actions/search.ts
@@ -1,3 +1,13 @@
 'use server';
 
-export async function search() {}
+import { redirect } from 'next/navigation';
+
+export async function search(formData: FormData) {
+  const term = formData.get('term');
+
+  if (typeof term !== 'string' || !term) {
+    redirect('/');
+  }
+
+  redirect(`/search?term=${term}`);
+}
--- components/search-input.tsx
+++ components/search-input.tsx
@@ -4,6 +4,7 @@ import { Input } from '@heroui/input';
 import { Kbd } from '@heroui/kbd';
 import { useSearchParams } from 'next/navigation';
 import { SearchIcon } from './icons';
+import * as actions from '@/actions';
 
 export default function SearchInput() {
   const searchParams = useSearchParams();
@@ -11,22 +12,25 @@ export default function SearchInput() {
   console.log('searchParams', searchParams);
 
   return (
-    <Input
-      defaultValue={searchParams.get('term') || ''}
-      aria-label="Search"
-      classNames={{
-        inputWrapper: 'bg-default-100',
-        input: 'text-sm',
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={['command']}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />}
-      type="search"
-    />
+    <form action={actions.search}>
+      <Input
+        defaultValue={searchParams.get('term') || ''}
+        aria-label="Search"
+        classNames={{
+          inputWrapper: 'bg-default-100',
+          input: 'text-sm',
+        }}
+        endContent={
+          <Kbd className="hidden lg:inline-block" keys={['command']}>
+            K
+          </Kbd>
+        }
+        labelPlacement="outside"
+        placeholder="Search..."
+        startContent={<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />}
+        type="search"
+        name="term"
+      />
+    </form>
   );
 }
