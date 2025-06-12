8c1a7d312dbeffe6ceaec98dd51bf440648f6159
Attila Horvath <sweeptic@gmail.com>
2025-06-12 12:51:35 +0200

    13. Merging a Few Files

--- app/topics/[slug]/posts/[postid]/page.tsx
+++ app/topics/[slug]/posts/[postid]/page.tsx
@@ -1,3 +1,20 @@
-export default function PostShowPage() {
-  return <div>Post Show Page</div>;
+import { paths } from '@/paths';
+import Link from 'next/link';
+
+export default async function PostShowPage({ params }: any) {
+  const { slug, postId } = await params;
+
+  return (
+    <div className="space-y-3">
+      <Link className="underline decoration-solid" href={paths.topicShow(slug)}>
+        {'< '}Back to {slug}
+      </Link>
+
+      {/* <PostShow /> */}
+
+      {/* <CommentCreateForm postId={postId} startOpen /> */}
+
+      {/* <CommentList comments={comments} /> */}
+    </div>
+  );
 }
